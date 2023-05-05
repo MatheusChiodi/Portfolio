49151a469528e80d063098e9ac1eb0f2f378e2a8
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-05 14:36:36 -0300

    add: ContainerThree

--- src/ContainerThree.jsx
+++ src/ContainerThree.jsx
@@ -1,35 +1,43 @@
 export default function ContainerThree() {
    
     return (
-        <div className="px-6 md:w-full w-[80%] mx-auto mt-10">
-            <div className="grid md:grid-cols-3 sm:grid-cols-1">
-                <div className=" border border-[#44475A] rounded-[10px] px-2">
-                    <h1 className="m-0 p-0 text-[50px] text-center w-full">
-                        <i className="fas fa-graduation-cap text-[#FF5555]"></i>
-                    </h1>
-                    <ul className="text-[16px] text-justify">
-                        <li className="text-[#44475A]">
-                            <p className="m-0 p-0">
-                                Instituto Federal de Educação, Ciência e Tecnologia de São Paulo IFSP
-                            </p>
-                            <span className="text-[#6272A4] text-[14px] m-0 p-0">Técnico em Informática </span>
-                        </li>
-                        <li className="text-[#FF5555] font-medium">
-                            <h1 className="m-0 p-0">
-                                Graduated in Systems Analysis and Development
-                            </h1>
-                        </li>
-                    </ul>
-                </div>
-                <div className="border border-[#44475A] rounded-[10px] px-10 md:mx-5 mx-0 md:my-0 my-6">
-                    <h1 className="m-0 p-0">
-                        fafafa
-                    </h1>
-                </div>
-                <div className="border border-[#44475A] rounded-[10px] px-10">
-                    <h1 className="m-0 p-0">
-                        fafafa
-                    </h1>
+        <div className="w-full lg:w-[90%] mx-auto m-0 p-0">
+            <div className="p-3 text-[#44475A] lg:text-[#F8F8F2] flex justify-center items-center">
+                <div className="flex justify-between flex-col lg:flex-row w-[100%] p-0 m-0">
+                    <div className="lg:bg-[#FF5555] w-full lg:w-4/12 border-r border-[#F8F8F2] m-0 p-3 rounded-tl-[50px]">
+                        <div className="text-start mb-2 mt-2 title">
+                            2017 - 2021
+                        </div>
+                        <p className="text-justify">
+                            Studying at the Federal Institute of Araraquara, I carried out projects such as my final course work, 
+                            which was based on creating an institutional website for the ONG Parque Vivo. The main objective was to provide quick 
+                            access to information on events and contacts with its members.
+                        </p>
+                    </div>
+                    <div className="lg:bg-[#FF5555] w-full lg:w-4/12 border-r border-[#F8F8F2] m-0 p-3">
+                        <div className="title text-end lg:text-center mb-2 mt-2">
+                            <span className="font2 fw-bold font-medium ps-1 lg:ps-0 bg-[#F8F8F2] lg:bg-[#FF5555]">
+                                2021 - Present
+                            </span>
+                        </div>
+                        <p className="text-justify">
+                            I worked as a full-stack developer at the company, creating institutional websites, developing file management systems, 
+                            creating platforms for LGPD-related issues, performing website maintenance, and improving management on e-commerce sites. 
+                            I also worked on search-related issues and possible bug fixes.
+                        </p>
+                    </div>
+                    <div className="lg:bg-[#FF5555] w-full lg:w-4/12 m-0 p-3 rounded-tr-[50px]">
+                        <div className="title text-center mb-2 mt-2">
+                            <span className="font2 fw-bold font-medium px-1 lg:ps-0 bg-[#F8F8F2] lg:bg-[#FF5555]">
+                                Infinit
+                            </span>
+                        </div>
+                        <p>
+                            I am a programmer who is constantly seeking to learn and improve my skills. 
+                            I am passionate about the field of programming and am always striving to expand my knowledge and take on new challenges. 
+                            With a strong drive to learn and a motivation to succeed, I am committed to being the best programmer I can be. 
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import Header from './templates/Header.jsx'
 import ContainerOne from './ContainerOne'
 import ContainerTwo from './ContainerTwo'
+import ContainerThree from './containerThree'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <ContainerOne />
     <ContainerTwo />
+    <ContainerThree />
   </React.StrictMode>,
 )
