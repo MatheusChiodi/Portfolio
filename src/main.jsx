f30541ef250699ef0dc42eca38c957d5b04c484b
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-05 09:38:58 -0300

    remove: ContainerOne

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './templates/Header.jsx'
-import ContainerOne from './pages/containerOne.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    <ContainerOne />
   </React.StrictMode>,
 )
