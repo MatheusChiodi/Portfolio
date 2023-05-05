4fc00fe4924caee308aff0a496d87bf9617bd575
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-05 09:36:58 -0300

    add: ContainerOne e ajuste Header

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     // se o tamanho da tela for menor que 1024 entao useState fica como false
     var status = false;
 
-    if (window.innerWidth > 1024) {
+    if (window.innerWidth > 992) {
         status = true;
     }
 
@@ -16,43 +16,52 @@ export default function Header() {
     }
 
     return (
-        <header>
-            <nav className="flex items-center justify-between flex-wrap  p-2 px-6 lg:border-none border-b border-b-[#44475A] rounded-b-[20px]">
-                <div className="block lg:hidden w-[100%]">
-                    <div className="flex justify-between flex-row w-[100%]">
-                        <div className="flex justify-cemter p-4 w-6/12">
-                            <p className="bg-[#FF5555] text-white w-[40px] h-[39px] mx-auto rounded-[80px] flex items-center justify-center">
-                                M
+        <header className="fixed top-0 w-full m-0 p-0 bg-white">
+            <nav className="flex items-center justify-between flex-wrap  px-6 md:border-none border-b border-b-[#44475A] rounded-b-[20px]">
+                <div className="block md:hidden w-[100%]">
+                    <div className="flex justify-between flex-row w-[100%] p-0 m-0">
+                        <div className="flex justify-center pt-1 p-0 m-0 w-3/12">
+                            <label className="hamburger mt-[-10px] p-0">
+                                <input type="checkbox" checked={isOpen} onChange={toggleMenu} />
+                                <svg viewBox="0 0 32 32">
+                                    <path className={isOpen ? 'line line-top-bottom open' : 'line line-top-bottom'} d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+                                    <path className={isOpen ? 'line open' : 'line'} d="M7 16 27 16"></path>
+                                </svg>
+                            </label>
+                        </div>
+                        <div className="flex justify-cemter pt-1 p-0 m-0 w-6/12">
+                            <p className="text-[#44475A] h-[39px] mx-auto rounded-[80px] flex items-center justify-center">
+                                <span className="text-[#FF5555] font-medium">&lt;</span>Matheus Chiodi <span className="text-[#FF5555]">/</span><span className="text-[#FF5555] font-medium"> &gt;</span>
                             </p>
                         </div>
-                        <div className="flex justify-center p-4 w-6/12">
-                            <button className="flex items-center px-3 py-2 border rounded text-gray border-[#44475A] hover:text-[#FF5555] hover:border-[#FF5555] transition-all" onClick={toggleMenu}>
-                                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                            </button>
+                        <div className="flex justify-cemter pt-1 p-0 m-0 w-3/12">
+                            <p className="text-[#44475A] h-[39px] mx-auto rounded-[80px] flex items-center justify-center">
+                            <a href="#" className="inline-block text-sm px-7 py-2 leading-none border rounded-xl border-[#44475A] text-white font-medium bg-gray">Contact</a>
+                            </p>
                         </div>
                     </div>
                     
                 </div>
                 {isOpen ? (
-                <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto" id="menu">
-                    <div className="lg:w-[50%] w-full text-center lg:text-left">
-                        <a href="#responsive-header" className="text-lg block mt-1 lg:mt-0 lg:inline-block text-gray font-medium lg:mr-4 mr-0">
+                <div className="w-full flex-grow md:flex md:items-center md:w-auto" id="menu">
+                    <div className="md:w-[60%] w-full text-center md:text-left md:mb-0 mb-2">
+                        <a href="#responsive-header" className="text-md block mt-1 md:mt-0 md:inline-block text-gray font-medium md:mr-4 mr-0">
                         About
                         </a>
-                        <a href="#responsive-header" className="text-lg block mt-1 lg:mt-0 lg:inline-block text-gray font-medium lg:mr-4 mr-0 mb-0">
+                        <a href="#responsive-header" className="text-md block mt-1 md:mt-0 md:inline-block text-gray font-medium md:mr-4 mr-0 mb-0">
                         Portfolio
                         </a>
-                        <a href="#responsive-header" className="text-lg block mt-1 lg:mt-0 lg:inline-block text-gray font-medium">
+                        <a href="#responsive-header" className="text-md block mt-1 md:mt-0 md:inline-block text-gray font-medium">
                         Experience
                         </a>
                     </div>
-                    <div className="px-2 w-[50%] text-center hidden lg:block">
+                    <div className="px-2 w-[30%] text-center hidden md:block">
                         <p className="bg-[#FF5555] text-white w-[40px] h-[39px] mx-auto rounded-[80px] flex items-center justify-center">
                             M
                         </p>
                     </div>
-                    <div className="lg:w-[50%] lg-[100%] text-center lg:text-right mt-2">
-                        <a href="#" className="inline-block text-sm px-7 py-1 leading-none border rounded-xl border-[#44475A] text-white font-medium bg-gray">Contact</a>
+                    <div className="md:w-[60%] md-[100%] text-center md:text-right mt-2 hidden md:block">
+                        <a href="#" className="inline-block text-sm px-7 py-2 leading-none border rounded-xl border-[#44475A] text-white font-medium bg-gray">Contact</a>
                     </div>
                 </div>
                 ) : (null)}
