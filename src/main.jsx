f4d75b76cdc8558c0f875939f8c9362c10502107
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-05 09:44:31 -0300

    add: ContainerOne

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './templates/Header.jsx'
+import ContainerOne from './ContainerOne'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <ContainerOne />
   </React.StrictMode>,
 )
