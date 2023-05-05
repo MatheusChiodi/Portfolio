adc8514c59e65aab1e3c7435751fb9c74d881e4d
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-05 09:33:21 -0300

    ajuste

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
 import Header from './templates/Header.jsx'
-import ContainerOne from './pages/containerOne.jsx'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    <ContainerOne />
+    <App />
   </React.StrictMode>,
 )
