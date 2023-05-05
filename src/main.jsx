a9c7d85088d463e36540b8adceac1f6d4d0b1cec
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-05 14:39:11 -0300

    remove: ContainerTwo

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './templates/Header.jsx'
 import ContainerOne from './ContainerOne'
-import ContainerThree from './containerThree'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <ContainerOne />
-    <ContainerThree />
   </React.StrictMode>,
 )
