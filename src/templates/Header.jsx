7a5d618767cf6d707c27e67deae99386a3142d9d
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-08 15:00:12 -0300

    add: ajuste button contact

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -48,8 +48,8 @@ export default function Header() {
                                     <img src='logo.png' className="w-[40px] h-[40px] ml-2" />
                                 </p>
                             </div>
-                            <div className="flex justify-cemter pt-1 p-0 m-0 w-5/12">
-                                <label htmlFor="toggleContactModal" className="inline-block px-4 py-1 leading-none border rounded-xl border-[#44475A] text-white text-[13px] bg-gray cursor-pointer">Contact</label>
+                            <div className="flex justify-center items-center pt-1 p-0 m-0 w-5/12">
+                                <label htmlFor="toggleContactModal" className="w-5/12 h-[25px] text-center inline-block px-4 py-1 leading-none border rounded-xl border-[#44475A] text-white text-[13px] bg-gray cursor-pointer">Contact</label>
                                 <input type="checkbox" name="toggleContactModal" id="toggleContactModal" checked={isOpenContact} onChange={toggleMenuContact}  className="hidden"/>
                             </div>
                         </div>
