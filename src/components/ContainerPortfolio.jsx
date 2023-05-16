1143b689414cb6b0c2231646262fab7bc7206d9d
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-16 15:01:05 -0300

    ajustes pastas

--- src/components/ContainerPortfolio.jsx
+++ src/components/ContainerPortfolio.jsx
@@ -6,11 +6,11 @@ export default function ContainerPortfolio() {
                     <div className="title text-center mb-2 mt-2">
                         <div className="border-[1.5px] border-[#FF5555] mb-[-17px]"></div>
                         <span className="font-bold font-lg px-1 bg-[#F8F8F2] lg:bg-slate-700 lg:text-[#F8F8F2] text-[#44475A] ">
-                            Projects
+                            My Projects
                         </span>
                     </div>
-                    <div className="grid lg:grid-cols-4 md:grid-cols-2 mx-auto m-0 p-0 w-full my-[5px] justify-center items-center">
-                        <div className="w-[250px] h-[250px] mb-[10px] lg:bg-white bg-slate-700 rounded-t-[20px] rounded-b-[20px] card">
+                    <div className="grid md:grid-cols-2 mx-auto m-0 p-0 w-full my-[5px] justify-center items-center">
+                        <div className="w-[250px] h-[250px] mb-[10px] lg:bg-white bg-slate-700 rounded-t-[20px] rounded-b-[20px] card mx-auto">
                             <div className="rounded-br-[40px] h-[140px] rounded-t-[20px] imgCard">
                                 <img src="./recriando_home_instagram.png" alt="image" className="w-full rounded-br-[40px] h-[140px] text-[#F8F8F2] rounded-t-[20px] rounded-b-[-200px]"/>
                             </div>
@@ -24,6 +24,27 @@ export default function ContainerPortfolio() {
                                     </p>
                                     <p className="restTextCard w-full text-center mt-4">
                                         <a href="https://matheuschiodi.github.io/Recriando-Home-Instagram/" target="_blank" rel="noopener noreferrer" className="text-[#F8F8F2] lg:text-[#44475A] hover:text-[#FF5555]">
+                                            <i className="fas fa-eye"></i>
+                                        </a>
+                                    </p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div className="w-[250px] h-[250px] mb-[10px] lg:bg-white bg-slate-700 rounded-t-[20px] rounded-b-[20px] card mx-auto">
+                            <div className="rounded-br-[40px] h-[140px] rounded-t-[20px] imgCard">
+                                <img src="./projectShop.png" alt="image" className="w-full rounded-br-[40px] h-[140px] text-[#F8F8F2] rounded-t-[20px] rounded-b-[-200px]"/>
+                            </div>
+                            <div className="rounded-br-[40px] h-[140px] lg:text-[#44475A] text-[#F8F8F2] rounded-b-[20px] textCard">
+                                <p className="w-full text-center font-medium text-[20px] text-[#FF5555] mt-2">
+                                    Project Shop
+                                </p>
+                                <div className="m-0 p-0 text-[16px]">
+                                    <p className="w-full text-center">
+                                    
+                                    </p>
+                                    <p className="restTextCard w-full text-center mt-4">
+                                        <a href="https://matheuschiodi.github.io/Project-Shop/" target="_blank" rel="noopener noreferrer" className="text-[#F8F8F2] lg:text-[#44475A] hover:text-[#FF5555]">
                                             <i className="fas fa-eye"></i>
                                         </a>
                                     </p>
--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './assets/styles/contact.css'
 import Header from './templates/Header.jsx'
-import ContainerAbout from './ContainerAbout'
-import ContainerExperience from './ContainerExperience'
 import Footer from './templates/Footer'
-import ContainerPortfolio from './ContainerPortfolio'
-
+import Home from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Header />
-        <ContainerAbout />
-        <ContainerPortfolio />
-        <ContainerExperience />
+        <Home />
         <Footer />
     </React.StrictMode>,
 )
+
