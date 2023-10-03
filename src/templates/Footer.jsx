be5c25abd41fa78149517917156888f3b64f0006
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-10-03 11:50:34 -0300

    Update Footer.jsx

--- src/templates/Footer.jsx
+++ src/templates/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
               <i className="fab fa-whatsapp-square"></i>
             </a>
 
-            <div className="bg-[#fff] h-[22px] w-[21.14px] mx-2 m-0 rounded-[2px] flex justify-center items-center hover:bg-[#FF5555]">
+            <div className="bg-[#fff] h-[20.1px] w-[21.14px] mx-2 m-0 rounded-[2px] flex justify-center items-center hover:bg-[#FF5555]">
               <a
                 href="https://play.google.com/store/apps/dev?id=8115131743129012258"
                 target="_blank"
@@ -48,7 +48,7 @@ const Footer = () => {
                 className="text-[#44475A]"
                 title="Google Play Store"
               >
-                <i className="fab fa-google-play text-[1rem] mb-[13.1px]"></i>
+                <i className="fab fa-google-play text-[0.9rem] mb-[14px]"></i>
               </a>
             </div>
           </div>
