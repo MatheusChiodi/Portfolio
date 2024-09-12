02102ec069b356b4818938996d2ff40825cfff76
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-09-12 12:57:20 -0300

    Revert "Ajuste no layout"
    This reverts commit 43175f3681b3ee35bbedc0a432345718ba04cc25.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,22 +27,6 @@ const Projects = ({ language }) => {
         (technology === 'HTML, CSS e JavaScript' && project.ti === 'HTML')
     );
 
-  window.addEventListener('scroll', reveal);
-
-  function reveal() {
-    var reveals = document.querySelectorAll('.reveal');
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add('active');
-      } else {
-        reveals[i].classList.remove('active');
-      }
-    }
-  }
-
   return (
     <>
       {isModalOpen && (
